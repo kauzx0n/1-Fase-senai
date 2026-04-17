3656816086e88ee5202c55a3fd20ea2a53d980af
kauzx0n <kauanpinheiropinto132@gmail.com>
2026-04-16 22:15:12 -0300

    eu programo em javascript/teste.js

---
+++ 01-javascript/testes.js
@@ -0,0 +1,23 @@
+// alert("Ola mundo!")
+// alert("SEGUNDA MENSAGEM")
+
+// let nome 
+
+// nome = "Kauan"
+
+// alert(nome)
+
+// let idade = 18
+// alert("idade: " + idade)
+
+// alert("Nome: " + nome + "\idade:" + idade")
+
+// nome = prompt("Digite seu nome:")
+// alert("Nome digitado: " + nome)
+
+
+let nota1
+nota1 = prompt("Digite a primeira nota: ")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert(nota1)
