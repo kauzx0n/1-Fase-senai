e0ecd404e40616134589c40565b7f5707b820a93
kauzx0n <kauanpinheiropinto132@gmail.com>
2026-05-13 21:45:08 -0300

    att

--- redes-kauan/script.js
+++ redes-kauan/script.js
@@ -4,7 +4,7 @@ const badge = document.getElementById('creator-badge');
 const music = document.getElementById('bg-music');
 const viewDisplay = document.getElementById('view-number');
 
-// 1. Lógica do Contador
+// Lógica do Contador
 let views = localStorage.getItem('kaka_views');
 if (!views) {
     views = 8271;
@@ -14,25 +14,24 @@ if (!views) {
 localStorage.setItem('kaka_views', views);
 viewDisplay.innerText = views.toLocaleString();
 
-// 2. Entrar no site e ativar animações
+// Entrar no site e ativar animações (e a MÚSICA)
 overlay.addEventListener('click', () => {
     overlay.style.opacity = '0';
     setTimeout(() => overlay.style.display = 'none', 800);
     
-    // Ativa o card
+    // Ativa o card e o selo
     card.classList.add('active');
-    
-    // Traz a palavra "Criado por Kaka" APENAS AGORA
     badge.classList.add('active');
     
-    // ZERA O DESFOQUE do fundo, deixando a imagem 100% nítida
+    // Zera o desfoque do fundo
     document.getElementById('background').style.filter = 'blur(0px) brightness(0.4)';
     
+    // TOCA A MÚSICA
     music.volume = 0.5;
     music.play();
 });
 
-// 3. Efeito Tilt Suave
+// Efeito Tilt Suave
 document.addEventListener('mousemove', (e) => {
     if (!card.classList.contains('active')) return;
     const xAxis = (window.innerWidth / 2 - e.pageX) / 45; 
