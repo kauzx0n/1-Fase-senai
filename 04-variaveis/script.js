fdfbfc84ecab933f51fcb07355276ac146970bad
kauzx0n <kauanpinheiropinto132@gmail.com>
2026-05-14 22:18:54 -0300

    att

---
+++ 04-variaveis/script.js
@@ -0,0 +1,28 @@
+let cont = 0
+function incrementarContagem(){
+    cont = cont + 1
+ document.getElementById('p-contagem').innerHTML = cont
+}
+
+
+
+
+
+
+
+let global = 'Eu sou uma variavel global :/'
+function funcao1(){
+  let local = 'sou uma variavel local :D'
+  console.log(local);
+  console.log(global);
+
+
+}
+function funcao2(){
+  console.log(global);
+// console.log(local);
+}
+
+
+// funcao1()
+// funcao2()
