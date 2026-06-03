e4792f52f5e101fef220d30f2e3ef0c05304db4e
kauzx0n <kauanpinheiropinto132@gmail.com>
2026-06-02 22:13:53 -0300

    botoesAprendendo

--- dino/script.js
+++ dino/script.js
@@ -1,6 +1,6 @@
 // Função simples e fácil de entender
 function jogar(dinoEscolhido) {
-    // 1. Pega o valor da aposta
+    //Pega o valor da aposta
     let aposta = document.getElementById("valorAposta").value;
     
     if (aposta === "") {
@@ -8,22 +8,22 @@ function jogar(dinoEscolhido) {
         return; // Para o código aqui se não tiver aposta
     }
 
-    // 2. Lista de todos os dinos possíveis
+    //Lista de todos os dinos possíveis
     let listaDeDinos = [
         "Espinossauro", "T-Rex", "Braquiossauro", 
         "Velocirraptor", "Estegossauro", "Triceratopo", 
         "Anquilossauro", "Parassaurolofo", "Alossauro", "Carnotauro"
     ];
 
-    // 3. A máquina sorteia um número de 0 a 9 e escolhe o dino dessa posição
+    //A máquina sorteia um número de 0 a 9 e escolhe o dino dessa posição
     let numeroAleatorio = Math.floor(Math.random() * 10);
     let dinoSorteado = listaDeDinos[numeroAleatorio];
 
-    // 4. Mostra os nomes na tela
+    //Mostra os nomes na tela
     document.getElementById("textoSeuDino").innerText = dinoEscolhido;
     document.getElementById("textoDinoSorteado").innerText = dinoSorteado;
 
-    // 5. Verifica se ganhou ou perdeu
+    //Verifica se ganhou ou perdeu
     let mensagem = document.getElementById("mensagemFinal");
     
     if (dinoEscolhido === dinoSorteado) {
